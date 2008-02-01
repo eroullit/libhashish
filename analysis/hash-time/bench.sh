b0dc08b4e51d4fa7eeaf73438ea2236292ec5e9f
Hagen Paul Pfeifer <hagen@jauu.net>
2008-02-01 20:50:40 +0000

    changed gnuplot style and add korzendorfer analysis

--- analysis/hash-time/bench.sh
+++ analysis/hash-time/bench.sh
@@ -2,7 +2,7 @@
 
 DATABYTES=$((1024*1024*1024))
 
-for name in goulburn hsieh jenkins phong ; do
+for name in goulburn hsieh jenkins phong korzendorfer1 ; do
 	echo "# $name; $DATABYTES total data per size"> bench-$name.data
 	for size in 1 3 4 8 9 16 31 32 64 ; do
 		./benchtool $name $DATABYTES $size >> bench-$name.data
